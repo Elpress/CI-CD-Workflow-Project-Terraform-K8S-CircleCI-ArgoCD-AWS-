a8eb07332f495207a3b2317c18b410179e01c518
Elvis <elvisbalo9@gmail.com>
2023-11-20 21:30:36 +0000

    Add EKS module

---
+++ modules/EKS/main.tf
@@ -0,0 +1,27 @@
+resource "aws_eks_cluster" "eks" {
+  #Cluster Name
+  name = var.PROJECT_NAME
+
+  role_arn = var.eks_cluster_arn
+
+  #K8S master version
+  version = "1.27"
+
+  vpc_config {
+    #Indicate whether or not the amazon EKS private API server enpoint is enabled
+    endpoint_private_access = false
+
+    #Indicate whether or not the amazon EKS private API server enpoint is enabled
+    endpoint_public_access = true
+
+    #Must be at least 2 different AZ
+    subnet_ids = [
+        var.var.PUB_SUB_1_A_ID,
+        var.PUB_SUB_2_B_ID,
+        var.PUB_SUB_3_A_ID,
+        var.PUB_SUB_4_B_ID
+    ]
+
+  }
+
+}
---
+++ modules/EKS/variables.tf
@@ -0,0 +1,17 @@
+variable "eks_cluster_arn" {
+}
+
+variable "PUB_SUB_1_A_ID" {
+}
+
+variable "PUB_SUB_2_B_ID" {
+}
+
+variable "PUB_SUB_3_A_ID" {
+}
+
+variable "PUB_SUB_4_B_ID" {
+}
+
+variable "PROJECT_NAME" {
+}
