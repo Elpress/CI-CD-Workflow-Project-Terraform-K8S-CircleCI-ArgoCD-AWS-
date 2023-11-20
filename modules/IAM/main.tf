88f171f07d25f28514c7c91771600d6151dc348b
Elvis <elvisbalo9@gmail.com>
2023-11-20 21:56:59 +0000

    Modules IAM added

---
+++ modules/IAM/main.tf
@@ -0,0 +1,90 @@
+resource "aws_iam_role" "eks_cluster_role" {
+  
+  #Role Name
+  name = "${var.PROJECT_NAME}-EKS-role"
+
+  assume_role_policy = <<POLICY
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "eks.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
+      }
+    ]
+  }
+
+  POLICY
+}
+
+resource "aws_idm_role_policy_attachement" "amazon_eks_cluster_policy" {
+  
+  # The ARN of the policy you want to apply
+  # https://github.com/SummitRoute/aws_managed_policies/blob/master/policies/AmazonEKSClusterPolicy
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+
+  role = aws_iam_role.eks_cluster_name
+}
+
+resource "aws_idm_role_policy_attachement" "elastic_loadbalancing_full_access" {
+  
+  policy_arn = "arn:aws:iam::aws:policy/ElasticLoadBalancingFullAccess"
+
+  role = aws_iam_role.eks_cluster_name
+}
+
+# Create IAM role for EKS Node Group
+
+resource "aws_iam_role" "nodes_general" {
+  # Role Name
+  name = "${var.PROJECT_NAME}-Node-group-role"
+
+  # The policy that grants an entity permission to assume the role.
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      }, 
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "amazon_eks_worker_node_policy_general" {
+  # The ARN of the policy you want to apply.
+  # https://github.com/SummitRoute/aws_managed_policies/blob/master/policies/AmazonEKSWorkerNodePolicy
+  
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+
+  # The role the policy should be applied to
+  role = aws_iam_role.nodes_general.name
+}
+
+resource "aws_iam_role_policy_attachment" "amazon_eks_cni_policy_general" {
+  # The ARN of the policy you want to apply.
+  # https://github.com/SummitRoute/aws_managed_policies/blob/master/policies/AmazonEKS_CNI_Policy
+  
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+
+  # The role the policy should be applied to
+  role = aws_iam_role.nodes_general.name
+}
+
+resource "aws_iam_role_policy_attachment" "amazon_ec2_container_registry_read_only" {
+  # The ARN of the policy you want to apply.
+  # https://github.com/SummitRoute/aws_managed_policies/blob/master/policies/AmazonEC2ContainerRegistryReadOnly
+  
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+
+  # The role the policy should be applied to
+  role = aws_iam_role.nodes_general.name
+}
