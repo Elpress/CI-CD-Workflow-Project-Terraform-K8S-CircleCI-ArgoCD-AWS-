0f3c2fb63ea1ce0e73ed65f21ae7018af3c7d3ed
Elvis <elvisbalo9@gmail.com>
2023-11-20 23:00:41 +0000

    Modules VPC added

---
+++ modules/VPC/variables.tf
@@ -0,0 +1,27 @@
+variable "region" {
+  
+}
+
+variable "PROJECT_NAME" {
+  
+}
+
+variable "vpc_cidr" {
+  
+}
+
+variable "PUB_SUB_1_A_CIDR" {
+  
+}
+
+variable "PUB_SUB_2_B_CIDR" {
+  
+}
+
+variable "PRI_SUB_3_A_CIDR" {
+  
+}
+
+variable "PRI_SUB_4_B_CIDR" {
+  
+}
